7856a1dc1847f8475c9ea72f8e6212e03fe7680a
abiel-sanch <abiel.sancho1@gmail.com>
2026-03-06 10:42:43 -0300

    #

--- lib/resumo.dart
+++ lib/resumo.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+
 
 class Resumo extends StatelessWidget {
   
-  const Resumo({super.key});
+  Resumo({super.key});
   //o que isso faz????
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(leading: IconButton(onPressed: () {Navigator.pop(context);}, icon: Icon(Icons.arrow_back)),),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('aqui vai ter o resumo de cada conteúdo e talvez questões pra responder?'),
-            Icon(Icons.my_library_books_sharp, size: 200),
-          ],
-        ),
-      ),
-    );
-  }
+
+  final _dadosStream = Supabase.instance.client.from('resumo').stream(primaryKey: ['id']);
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('Teste'),),
+    body: StreamBuilder<List<Map<String, dynamic>>>(
+    stream: _dadosStream,
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) {
+        return const Center(child: CircularProgressIndicator(),);
+      }
+      final dados = snapshot.data!;
+
+      return ListView.builder(
+        itemCount: dados.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(dados[index]['texto']),
+          );
+        },
+      );
+    },
+  )
+  );
+}
 }
